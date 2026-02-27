7b4fd62fad1ecc04c100d4eb11e3ce9c4917aee8
asmita <164854057+asmitajarhalli123@users.noreply.github.com>
2026-02-28 00:00:45 +0530

    Add files via upload

---
+++ src/components/navigation/navigation.jsx
@@ -0,0 +1,37 @@
+import './navigation.css'
+
+
+const Navigation = ()=>{
+
+    return (
+        <>
+       <div className="hero">
+        <div className="navbar">
+
+            <div className="left">
+          <h1>@smiForge</h1>
+            </div>
+
+        <div className="right">
+            <a href="">About</a>
+            <a href="">Skills</a>
+            <a href="">Project</a>
+            <a href="">Contact</a>
+          </div>
+        </div>
+
+        <div className="conatiner">
+        <div className="container1">
+             
+        </div>
+      
+        </div>
+
+
+
+       </div>
+       </>
+    );
+}
+
+export default Navigation;
